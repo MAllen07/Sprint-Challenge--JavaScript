27174ca5c261180f1634cb78a75ccc15c0a29c0f
Mindy Allen <mrsmallen07@gmail.com>
2019-10-14 20:12:54 -0400

    functions complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,14 +1,13 @@
 // ==== Callbacks ====  
 
-/* Step 1: Create a higher-order function that accepts a callback
-  * Create a higher-order function named consume that can take 3 parameters.
-  * The first two parameters can accept any argument
-  * The last parameter accepts a callback 
-  * In the body of the function return the callback with the two parameters that you created
+/* Step 1: Create a higher-order function
+  * Create a higher-order function named consume with 3 parameters: a, b and cb
+  * The first two parameters can take any argument (we can pass any value as argument)
+  * The last parameter accepts a callback
+  * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
-function consume (p1, p2, cb) {
-  return cb(p1, p2);
+let consume = function(a, b, cb){
+  return cb(a,b);
 }
 
 
@@ -17,33 +16,32 @@ function consume (p1, p2, cb) {
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-function add(x, y) {
-  return x + y;
+
+const add = function(a,b){
+  return a + b;
+} 
+
+const multiply = function(a,b){
+  return a * b;
 }
 
-function multiply(x, y) {
-  return x * y;
+const greeting = function(first,last){
+  return `Hello ${first} ${last}, nice to meet you!`;
 }
-
-function greeting(first, last) {
-  return(`Hello ${first} ${last}, nice to meet you!`);
-
-
-
-
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-consume(2,2,add); // 4
-consume(10,16,multiply); // 160
-consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
-// Explain in your own words why `nestedfunction()` can access the variable `internal`.
+// Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
-//nested functions can access the variables of the enclosing ScopedCredential.
+// Explanation: The variable 'internal' is created in the 'myFunction' function. The 'nestedFunction' as its name suggests, is nested within the 'myFunction' function along with the variable 'internal'. While 'internal' is not global in the page, it is higher order than 'nestedFunction' and in the same lexical environment, so the value of 'internal' is available to 'nestedFunction'.
+
+
 
 const external = "I'm outside the function";
 
